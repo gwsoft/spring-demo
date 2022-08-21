a3df73182601ca64addc286d7bf2e85a0ddcee77
Grzegorz Wieczerzak <gwsoft@particle.pl>
2022-08-21 17:24:22 +0200

    Added CarReponse class and a sample method in the Controller

--- CarsDealerOnline/src/main/java/pl/particle/gwsoft/controller/CarController.java
+++ CarsDealerOnline/src/main/java/pl/particle/gwsoft/controller/CarController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import pl.particle.gwsoft.response.CarResponse;
+
 @RestController
 @RequestMapping("/api/car")
 public class CarController {
 
   @GetMapping("/dummy")
-  public String getCar() {
+  public String getDummyCar() {
     return "dummy car";
   }
   
@@ -23,5 +25,11 @@ public class CarController {
   @GetMapping("/appInfo")
   public String getAppInfo() {
     return "Welcome in " + appName + "<br>" + "author: " + appAuthor;
+  }
+  
+  @GetMapping("/get")
+  public CarResponse getCar() {
+    CarResponse carResponse = new CarResponse(1, "hatchback", "Volvo", "silver", 10000, "used", 2015, 45000.0);
+    return carResponse;
   }
 }
